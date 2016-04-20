8b94d10b5f20bd0484e904536804ddc0ad545664
Max Spencer <max.spencer@guardian.co.uk>
2016-04-20 11:47:54 +0100

    Search route which takes search term and a param

--- tweeter.py
+++ tweeter.py
@@ -73,15 +73,35 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    return "hello"
+
+@app.route("/search/<term>")
+def search(term):
     url = "https://api.twitter.com/1.1/search/tweets.json?" + urllib.urlencode({
-        "q": "porcupine",
-        "count": 4,
+        "q": urllib.quote(term),
         "result_type": "mixed"
     })
     print(url)
     search_req = new_authenticated_request(url)
     search_response = urllib2.urlopen(search_req)
-    return search_response.read()
+    search_data = json.loads(search_response.read())
+
+    # Build HTML to return
+    head_html = "<head><title>Search: " + term + "</title></head>"
+
+    # Processing the tweets themselves
+    tweet_html = "<ul>"
+    for tweet in search_data["statuses"]:
+        tweet_html += "<li>" + tweet["text"] + "</li>"
+    tweet_html += "</ul>"
+
+    # Wrapping up the tweet_html into a HTML body with a title
+    body_html = "<body><h1>Search: " + term + "</h1>" + tweet_html + "</body>"
+
+    # Join the head and body together
+    html = "<html>" + head_html + body_html + "</html>"
+        
+    return html
 
 
 if __name__ == "__main__":
